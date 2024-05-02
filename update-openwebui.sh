f6523107e4e0aa29806b312ce1363d8803c4a0c6
Kyuonixo <it@ancoro.cat>
2024-05-02 02:34:15 +0000

    Update on script "update-openwebui.sh"

--- update-openwebui.sh
+++ update-openwebui.sh
@@ -11,4 +11,8 @@ npm run build
 pip install -r ./backend/requirements.txt -U
 
 echo "Open WebUI has been updated!"
+
+sleep 3
+
+# Restarting the service
 echo "Now you can restart the service to apply changes."
